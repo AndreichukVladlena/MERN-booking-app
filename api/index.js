5c1cc74d934ca79f050149f65b50ab09d101c6d0
vlanuka <andreychuk04@mail.ru>
2024-08-06 16:42:02 +0300

    feat: add cookie

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'jhnfkddjdiguoie84i';
 
 app.use(express.json());
 app.use(cors({
@@ -34,8 +36,24 @@ app.post('/register', async (req, res) => {
         res.status(422).json(e);
     }
    
+});
 
-
+app.post('/login', async (req, res) => {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk){
+            jwt.sign({email: userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token) => {
+                if(err) throw err;
+                res.cookie('token', token).json('password ok');
+            })
+        } else {
+            res.status(422).json('password not ok');
+        }
+    } else {
+        res.json('not found');
+    }
 });
 
 app.listen(4000);
