e406215888c7cd1af42dac6cf6e24137fd5b7e81
vlanuka <andreychuk04@mail.ru>
2024-08-08 12:18:08 +0300

    feat: AccountPage created

---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
+import { Navigate } from "react-router-dom";
+
+export default function AccountPage (){
+    const {ready, user} = useContext(UserContext);
+
+    if (!ready){
+        return <div>Loading...</div>;
+    }
+
+    if (ready && !user){
+        return <Navigate to={'/login'}/>
+    }
+
+    return (
+        <div>
+            'account for {user.name}'
+        </div>
+    );
+}
