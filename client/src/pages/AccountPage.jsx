264dcaf4102acdde8ff3065722e0950f3ca7f0d8
vlanuka <andreychuk04@mail.ru>
2024-08-08 13:33:10 +0300

    feat: add nav

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,15 @@
 import { useContext } from "react";
 import { UserContext } from "../UserContext";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountPage (){
     const {ready, user} = useContext(UserContext);
+
+    let {subpage} = useParams();
+    if (subpage === undefined){
+        subpage = 'profile';
+    }
 
     if (!ready){
         return <div>Loading...</div>;
@@ -13,9 +19,31 @@ export default function AccountPage (){
         return <Navigate to={'/login'}/>
     }
 
+    async function logout (){
+        await axios.post('/logout');
+    }
+    
+    function linkClasses(type=null){
+        let classes =  'py-2 px-6';
+        if(type === subpage || (subpage === undefined && type === 'profile')){
+            classes += ' bg-primary text-white rounded-full';
+        } 
+        return classes;
+    }
+
     return (
         <div>
-            'account for {user.name}'
+            <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+                <Link className={linkClasses('profile')} to={'/account'}>My profile</Link>
+                <Link className={linkClasses('bookings')} to={'/account/bookings'}>My bookings</Link>
+                <Link className={linkClasses('places')}  to={'/account/places'}>My accomodations</Link>
+            </nav>
+            {subpage === 'profile' && (
+                <div className="text-center">
+                    Logged in as {user.name} ({user.email})<br />
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                </div>
+            )}
         </div>
     );
 }
