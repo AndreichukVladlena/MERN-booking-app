f8c9158fa2354c2ad87c24cda4a8af016bc6e25c
vlanuka <andreychuk04@mail.ru>
2024-08-17 13:50:31 +0300

    refactor: places form moved into separated component

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,37 +1,8 @@
-import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import Perks from "../Perks";
-import PhotosUploader from "../PhotosUploader";
-import axios from "axios";
+import { Link, useParams, Navigate } from "react-router-dom";
+import PlacesFormPage from "./PlacesFormPage";
 
 export default function PlacesPage() {
   const { action } = useParams();
-  const [title, setTitle] = useState("");
-  const [address, setAddress] = useState("");
-  const [description, setDescription] = useState("");
-  const [perks, setPerks] = useState([]);
-  const [extraInfo, setExtraInfo] = useState("");
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
-  const [maxGuests, setMaxGuests] = useState(1);
-  const [addedPhotos, setAddedPhotos] = useState([]);
-
-  function inputHeader(text) {
-    return <h2 className="text-2xl mt-4">{text}</h2>;
-  }
-
-  function inputDescription(text) {
-    return <p className="text-gray-500 text-sm">{text}</p>;
-  }
-
-  function preInput(header, description) {
-    return (
-      <>
-        {inputHeader(header)}
-        {inputDescription(description)}
-      </>
-    );
-  }
 
   return (
     <div>
@@ -59,82 +30,7 @@ export default function PlacesPage() {
           </Link>
         </div>
       )}
-      {action === "new" && (
-        <div>
-          <form>
-            {preInput(
-              "Title",
-              "Title for your place, should be short and catchy"
-            )}
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="title, for example: My lovely appartment"
-            />
-            {preInput("Address", "Address to this place")}
-            <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              placeholder="address"
-            />
-            {preInput("Photos", "more = better")}
-            <PhotosUploader
-              addedPhotos={addedPhotos}
-              onChange={setAddedPhotos}
-            />
-            {preInput("Description", "description of the place")}
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            {preInput("Perks", "select all the perks of your place")}
-            <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-              <Perks selected={perks} onChange={setPerks} />
-            </div>
-            {preInput("Extra info", "house rules, etc")}
-            <textarea
-              value={extraInfo}
-              onChange={(e) => setExtraInfo(e.target.value)}
-            />
-            {preInput(
-              "Check in&out times",
-              "add check in and out times, remember to have some time window for cleaning the room between guests"
-            )}
-            <div className="grid gap-2 sm:grid-cols-3">
-              <div>
-                <h3 className="mt-2 -mb-1">Check in time</h3>
-                <input
-                  type="text"
-                  value={checkIn}
-                  onChange={(e) => setCheckIn(e.target.value)}
-                  placeholder="14"
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 -mb-1">Check out time</h3>
-                <input
-                  type="text"
-                  value={checkOut}
-                  onChange={(e) => setCheckOut(e.target.value)}
-                  placeholder="11"
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 -mb-1">Max number of guests</h3>
-                <input
-                  type="number"
-                  value={maxGuests}
-                  onChange={(e) => setMaxGuests(e.target.value)}
-                  placeholder="20"
-                />
-              </div>
-            </div>
-            <button className="primary my-4">Save</button>
-          </form>
-        </div>
-      )}
+      {action === "new" && <PlacesFormPage />}
     </div>
   );
 }
