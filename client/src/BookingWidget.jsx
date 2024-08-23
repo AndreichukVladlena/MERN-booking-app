fd8ebafe69a5ca8650929a474ef7c2e605ae71f9
vlanuka <andreychuk04@mail.ru>
2024-08-23 19:10:34 +0300

    feat: add states

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
 export default function BookingWidget({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
@@ -8,17 +12,29 @@ export default function BookingWidget({ place }) {
         <div className="flex">
           <div className="py-3 px-4">
             <label>Check in: </label>
-            <input type="date" />
+            <input
+              type="date"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+            />
           </div>
           <div className="py-3 px-4 border-l">
             <label>Check out: </label>
-            <input type="date" />
+            <input
+              type="date"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+            />
           </div>
         </div>
       </div>
       <div className="py-3 px-4 border-t">
         <label>Number of guests: </label>
-        <input type="number" value={1} />
+        <input
+          type="number"
+          value={numberOfGuests}
+          onChange={(ev) => setNumberOfGuests(ev.target.value)}
+        />
       </div>
       <button className="primary mt-4">Book this place</button>
     </div>
