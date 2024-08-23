1613e52bc1ccf8cbfa9b09052c24dd58297d7405
vlanuka <andreychuk04@mail.ru>
2024-08-23 18:53:34 +0300

    feat: add extra info to PlacePage

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -57,7 +57,7 @@ export default function PlacePage() {
     );
   }
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         className="flex gap-1 my-3 block font-semibold underline"
@@ -132,7 +132,7 @@ export default function PlacePage() {
           Show more photos
         </button>
       </div>
-      <div className="mt-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div>
           <div className="my-4">
             <h2 className="font-semibold text-2xl">Description</h2>
@@ -144,6 +144,14 @@ export default function PlacePage() {
         </div>
         <div>
           <BookingWidget place={place} />
+        </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra info:</h2>
+        </div>
+        <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">
+          {place.extraInfo}
         </div>
       </div>
     </div>
