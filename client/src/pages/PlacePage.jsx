1047b70bcba38987eabd0ec290e2ee7bbc10cfe3
vlanuka <andreychuk04@mail.ru>
2024-08-23 19:04:03 +0300

    feat: styles changed

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -22,7 +22,7 @@ export default function PlacePage() {
       <div className="absolute inset-0 bg-black text-white min-h-screen">
         <div className="bg-black p-8 grid gap-4">
           <div>
-            <h2 className="text-3xl">Photos of {place.title}</h2>
+            <h2 className="text-3xl mr-36">Photos of {place.title}</h2>
             <button
               onClick={() => setShowAllPhotos(false)}
               className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
@@ -89,22 +89,27 @@ export default function PlacePage() {
         <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
           <div>
             {place.photos?.[0] && (
-              <div className="aspect-square object-cover">
-                <img src={"http://localhost:4000/uploads/" + place.photos[0]} />
+              <div className="aspect-square object-cover cursor-pointer">
+                <img
+                  onClick={() => setShowAllPhotos(true)}
+                  src={"http://localhost:4000/uploads/" + place.photos[0]}
+                />
               </div>
             )}
           </div>
           <div className="grid">
             {place.photos?.[1] && (
               <img
-                className="aspect-square object-cover"
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square object-cover cursor-pointer"
                 src={"http://localhost:4000/uploads/" + place.photos[1]}
               />
             )}{" "}
             <div className="overflow-hidden">
               {place.photos?.[2] && (
                 <img
-                  className="aspect-square object-cover relative top-2"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square object-cover cursor-pointer relative top-2"
                   src={"http://localhost:4000/uploads/" + place.photos[2]}
                 />
               )}
