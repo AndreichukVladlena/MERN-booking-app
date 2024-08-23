7bdaa2ef670485bfbf4cf9d7dfabc1a08b52bef6
vlanuka <andreychuk04@mail.ru>
2024-08-23 19:20:23 +0300

    feat: add price counter using date-fns

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
+import { differenceInCalendarDays } from "date-fns";
 export default function BookingWidget({ place }) {
   const [checkIn, setCheckIn] = useState("");
   const [checkOut, setCheckOut] = useState("");
   const [numberOfGuests, setNumberOfGuests] = useState(1);
+  let numberOfNights = 0;
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
+    );
+  }
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
@@ -36,7 +44,10 @@ export default function BookingWidget({ place }) {
           onChange={(ev) => setNumberOfGuests(ev.target.value)}
         />
       </div>
-      <button className="primary mt-4">Book this place</button>
+      <button className="primary mt-4">
+        Book this place
+        {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
+      </button>
     </div>
   );
 }
