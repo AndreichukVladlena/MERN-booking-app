fa29f2b8ebaf2dae207ce6bb33de4d3d8848cfa4
vlanuka <andreychuk04@mail.ru>
2024-08-24 16:41:52 +0300

    fix: add ref to owner

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-  place: { type: mongoose.Schema.Types.ObjectId, required: true },
+  place: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Place" },
   checkIn: { type: Date, required: true },
   checkOut: { type: Date, required: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: Number },
 });
 
 const BookingModel = mongoose.model("Booking", bookingSchema);
