9358a5ea36bb447a462c3ca5617cdc0a5c2d4ecc
vlanuka <andreychuk04@mail.ru>
2024-08-24 16:40:51 +0300

    fix: fixed problem with calling function bookThisPlace

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { differenceInCalendarDays } from "date-fns";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 export default function BookingWidget({ place }) {
   const [checkIn, setCheckIn] = useState("");
   const [checkOut, setCheckOut] = useState("");
   const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [redirect, setRedirect] = useState("");
+
   let numberOfNights = 0;
   if (checkIn && checkOut) {
     numberOfNights = differenceInCalendarDays(
@@ -11,6 +17,25 @@ export default function BookingWidget({ place }) {
       new Date(checkIn)
     );
   }
+
+  async function bookThisPlace() {
+    const response = await axios.post("/bookings", {
+      checkIn,
+      checkOut,
+      numberOfGuests,
+      name,
+      phone,
+      place: place._id,
+      price: numberOfNights * place.price,
+    });
+    const bookingId = response.data._id;
+    setRedirect(`/account/bookings/${bookingId}`);
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
@@ -44,7 +69,23 @@ export default function BookingWidget({ place }) {
           onChange={(ev) => setNumberOfGuests(ev.target.value)}
         />
       </div>
-      <button className="primary mt-4">
+      {numberOfNights > 0 && (
+        <div className="py-3 px-4 border-t">
+          <label>Your full name: </label>
+          <input
+            type="text"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <label>Phone number: </label>
+          <input
+            type="tel"
+            value={phone}
+            onChange={(ev) => setPhone(ev.target.value)}
+          />
+        </div>
+      )}
+      <button onClick={bookThisPlace} className="primary mt-4">
         Book this place
         {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
       </button>
