97c1aacc289f3fb46f2407bf82fff9a417ade3ee
vlanuka <andreychuk04@mail.ru>
2024-08-25 15:18:58 +0300

    feat: places address moved into separated component

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import BookingWidget from "../BookingWidget";
 import PlaceGallery from "../PlaceGallery";
+import AddressLink from "../AddressLink";
 export default function PlacePage() {
   const { id } = useParams();
   const [place, setPlace] = useState(null);
@@ -20,32 +21,7 @@ export default function PlacePage() {
   return (
     <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
-      <a
-        className="flex gap-1 my-3 block font-semibold underline"
-        target="_blank"
-        href={"https://maps.google.com?q=" + place.address}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-          />
-        </svg>
-        {place.address}
-      </a>
+      <AddressLink>{place.address}</AddressLink>
       <PlaceGallery place={place} />
       <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div>
